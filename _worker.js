37c0aaad7ca27c6f034783b505a5cac0412af909
Liangbin Lian <1129525450@qq.com>
2023-11-26 21:25:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
 
 /**
  * Respond to the request
@@ -8,83 +8,83 @@ addEventListener("fetch", (event) => {
  */
 async function handleRequest(request) {
   const replace_wikimedia = function(val) {
-    return val.replaceAll(/(https:)?\/\/upload.wikimedia.org/g, "/upload.wikimedia.org")
-  }
+    return val.replaceAll(/(https:)?\/\/upload.wikimedia.org/g, "/upload.wikimedia.org");
+  };
   class ElementHandler {
     constructor() {
-      this.inCss = false
-      this.cssContent = undefined
+      this.inScript = false;
+      this.sctiptContent = undefined;
     }
     element(element) {
-      ["src", "srcset", "href", "poster", "content"].forEach(function(key) {
-        var val = element.getAttribute(key)
+      ["src", "srcset", "href", "poster", "content", "data-src", "data-srcset"].forEach(function(key) {
+        var val = element.getAttribute(key);
         if (val !== null) {
-          element.setAttribute(key, replace_wikimedia(val))
+          element.setAttribute(key, replace_wikimedia(val));
         }
-      })
-      if (element.tagName === 'style') {
-        this.inCss = true
-        var self = this
-        this.cssContent = ""
+      });
+      if (element.tagName === "style") {
+        this.inScript = true;
+        var self = this;
+        this.sctiptContent = "";
         element.onEndTag(function() {
-          self.inCss = false;
-          self.cssContent = undefined
-        })
+          self.inScript = false;
+          self.sctiptContent = undefined;
+        });
       }
     }
     text(text) {
-      if (this.inCss) {
-        this.cssContent += text.text
+      if (this.inScript) {
+        this.sctiptContent += text.text;
         if (text.lastInTextNode) {
-          text.replace(replace_wikimedia(this.cssContent))
+          text.replace(replace_wikimedia(this.sctiptContent), {html: true});
         } else {
-          text.remove()
+          text.remove();
         }
       }
     }
   }
   try {
-    var u = new URL(request.url)
+    var u = new URL(request.url);
     if (u.protocol !== "https:") {
-      u.protocol = "https:"
-      return new Response("", { status: 301, headers: { "Location": u.href } })
+      u.protocol = "https:";
+      return new Response("", { status: 301, headers: { "Location": u.href } });
     }
-    //u.searchParams.append('variant', 'zh-cn')
+    //u.searchParams.append('variant', 'zh-cn');
     if (u.pathname === "/") {
-      return new Response("", { status: 302, headers: { "Location": "/wiki/" } })
+      return new Response("", { status: 302, headers: { "Location": "/wiki/" } });
     }
-    var d = null
-    var res = null
+    var d = null;
+    var res = null;
     if (u.pathname.startsWith("/upload.wikimedia.org/")) {
-      var target = `https:/${u.pathname}${u.search}`
-      res = await fetch(new Request(target, request))
+      var target = `https:/${u.pathname}${u.search}`;
+      res = await fetch(new Request(target, request));
     } else {
-      var ua = request.headers.get("User-Agent")
-      var ual = (ua || "").toLowerCase()
-      var target = `https://zh${ual.indexOf("mobile") !== -1 || ual.indexOf("android") !== -1 || ual.indexOf("like mac os x") !== -1 ? ".m" : ""}.wik${"ip"}edia.org${u.pathname}${u.search}`
-      d = await fetch(new Request(target, request))
+      var ua = request.headers.get("User-Agent");
+      var ual = (ua || "").toLowerCase();
+      var target = `https://zh${ual.indexOf("mobile") !== -1 || ual.indexOf("android") !== -1 || ual.indexOf("like mac os x") !== -1 ? ".m" : ""}.wik${"ip"}edia.org${u.pathname}${u.search}`;
+      d = await fetch(new Request(target, request));
       if (u.pathname === "/wiki/" && d.status == 301) {
-        var red = new URL(d.headers.get('Location'));
+        var red = new URL(d.headers.get("Location"));
         return new Response("", { status: 301, headers: { "Location": `${red.pathname}${red.search}` } });
       }
-      var resptype = d.headers.get("Content-Type")
+      var resptype = d.headers.get("Content-Type");
       if (resptype.startsWith("text/html")) {
-        res = new HTMLRewriter().on("*", new ElementHandler()).transform(d)
+        res = new HTMLRewriter().on("*", new ElementHandler()).transform(d);
       } else if ((resptype.startsWith("application/json") && u.pathname === "/w/api.php") ||
         (resptype.startsWith("text/javascript") && u.pathname === "/w/load.php")) {
-        const decoder = new TextDecoder("utf-8")
-        const encoder = new TextEncoder()
+        const decoder = new TextDecoder("utf-8");
+        const encoder = new TextEncoder();
         const { readable, writable } = new TransformStream({ transform: function(v, ctl) {
-          ctl.enqueue(encoder.encode(replace_wikimedia(decoder.decode(v))))
+          ctl.enqueue(encoder.encode(replace_wikimedia(decoder.decode(v))));
         }})
-        d.body.pipeTo(writable)
-        res = new Response(readable, { status: d.status, headers: d.headers })
+        d.body.pipeTo(writable);
+        res = new Response(readable, { status: d.status, headers: d.headers });
       } else {
-        res = d
+        res = d;
       }
     }
-    return res
+    return res;
   } catch (e) {
-    return new Response(String(e), { status: 500 })
+    return new Response(String(e), { status: 500 });
   }
 }
