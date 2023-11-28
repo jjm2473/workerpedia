50cb2effb9ad72c2cdca59f8f7b43b89fe48ed89
Liangbin Lian <1129525450@qq.com>
2023-11-28 11:05:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,7 +8,10 @@ addEventListener("fetch", (event) => {
  */
 async function handleRequest(request) {
   const replace_wikimedia = function(val) {
-    return val.replaceAll(/(https:)?\/\/upload.wikimedia.org/g, "/upload.wikimedia.org");
+    return val.replaceAll(/(https:)?\/\/upload\.wikimedia\.org/g, "/upload.wikimedia.org");
+  };
+  const replace_metawiki = function(val) {
+    return val.replaceAll(/(https:)?\/\/meta\.wikimedia\.org\//g, "/meta.wikimedia.org/");
   };
   class ElementHandler {
     constructor() {
@@ -44,6 +47,10 @@ async function handleRequest(request) {
     }
   }
   try {
+    if (request.method != "GET" && request.method != "HEAD") {
+      return new Response("405 Method Not Allowed", { status: 405, headers: { "Content-Type": "text/plain",
+                          "Etag": "abcdefgh", "Cache-Control": "public, max-age=86400" } });
+    }
     var u = new URL(request.url);
     if (u.protocol !== "https:") {
       u.protocol = "https:";
@@ -55,13 +62,18 @@ async function handleRequest(request) {
     }
     var d = null;
     var res = null;
+    var target = null;
     if (u.pathname.startsWith("/upload.wikimedia.org/")) {
-      var target = `https:/${u.pathname}${u.search}`;
+      target = `https:/${u.pathname}${u.search}`;
       res = await fetch(new Request(target, request));
     } else {
-      var ua = request.headers.get("User-Agent");
-      var ual = (ua || "").toLowerCase();
-      var target = `https://zh${ual.indexOf("mobile") !== -1 || ual.indexOf("android") !== -1 || ual.indexOf("like mac os x") !== -1 ? ".m" : ""}.wik${"ip"}edia.org${u.pathname}${u.search}`;
+      if (u.pathname.startsWith("/meta.wikimedia.org/")) {
+        target = `https:/${u.pathname}${u.search}`;
+      } else {
+        var ua = request.headers.get("User-Agent");
+        var ual = (ua || "").toLowerCase();
+        target = `https://zh${ual.indexOf("mobile") !== -1 || ual.indexOf("android") !== -1 || ual.indexOf("like mac os x") !== -1 ? ".m" : ""}.wik${"ip"}edia.org${u.pathname}${u.search}`;
+      }
       d = await fetch(new Request(target, request));
       if (u.pathname === "/wiki/" && d.status == 301) {
         var red = new URL(d.headers.get("Location"));
@@ -70,12 +82,15 @@ async function handleRequest(request) {
       var resptype = d.headers.get("Content-Type");
       if (resptype.startsWith("text/html")) {
         res = new HTMLRewriter().on("*", new ElementHandler()).transform(d);
-      } else if ((resptype.startsWith("application/json") && u.pathname === "/w/api.php") ||
-        (resptype.startsWith("text/javascript") && u.pathname === "/w/load.php")) {
+      } else if ((resptype.startsWith("application/json") &&
+                  (u.pathname === "/w/api.php" || u.pathname === "/w/rest.php/v1/search/title")) ||
+        (resptype.startsWith("text/javascript") &&
+          (u.pathname === "/w/load.php" || u.pathname === "/w/index.php"
+           || u.pathname === "/meta.wikimedia.org/w/index.php" ))) {
         const decoder = new TextDecoder("utf-8");
         const encoder = new TextEncoder();
         const { readable, writable } = new TransformStream({ transform: function(v, ctl) {
-          ctl.enqueue(encoder.encode(replace_wikimedia(decoder.decode(v))));
+          ctl.enqueue(encoder.encode(replace_wikimedia(replace_metawiki(decoder.decode(v)))));
         }})
         d.body.pipeTo(writable);
         res = new Response(readable, { status: d.status, headers: d.headers });
